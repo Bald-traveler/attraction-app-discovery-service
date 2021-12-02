770540709d049b3b43a973657125495fa2d08e90
heathdj <heathdj@gmail.com>
2021-12-02 13:48:34 -0500

    Initial Commit

---
+++ src/main/java/com/baldtraveler/attractionsappdiscoveryservice/AttractionsappdiscoveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.baldtraveler.attractionsappdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionsappdiscoveryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttractionsappdiscoveryserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baldtraveler/attractionsappdiscoveryservice/AttractionsappdiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baldtraveler.attractionsappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionsappdiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
